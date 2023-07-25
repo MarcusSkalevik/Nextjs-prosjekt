5eefb5e8dacadfd3453b4a026e2f45303361f4e0
MarcusSkalevik <marcusandreasskaal@gmail.com>
2023-07-25 19:04:45 +0200

    c

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from '../books.module.css';
 import Link from 'next/link';
 
+
 const NavBar = () => {
   return (
+    
     <div className={styles.navBar}>
       <Link href="https://www.google.com/">
         <button className={styles.navButton}>
