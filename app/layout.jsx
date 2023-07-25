5a1aa8ee25e087c3e5ea894021a43ea29161f9c5
MarcusSkalevik <marcusandreasskaal@gmail.com>
2023-07-25 14:01:04 +0200

    typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Marcus',
-  description: 'None',
+  description: 'Ultimate Bookstore',
 }
 
 export default function RootLayout({ children }) {
