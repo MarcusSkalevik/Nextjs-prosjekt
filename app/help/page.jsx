76e4d4edaf7507b524590850e032747f41f39858
MarcusMarcusMarcusMarcusMarcus <marcus@ilder.no>
2023-08-17 08:23:51 +0200

    d

--- app/help/page.jsx
+++ app/help/page.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../books.module.css'; 
 import NavBar from '../components/navbar';
 import listedBooks from '../components/bookdata';
 import SiteFooter from '../components/siteFooter';
 import Product from '../components/product';
+import styles from '../help.module.css';
 
-const Help = () => {
+const About = () => {
     return (
-    <h1>Help</h1>
-
-
-
-
-
+        <main className={styles['bg-image']}>
+    <div className={styles['bg-gradient-to-br']}>
+        <h1 className={styles.mainTitle}>Help</h1>
+    </div>
+    </main>
     );
 };
 
-export default Help;
+export default About;
